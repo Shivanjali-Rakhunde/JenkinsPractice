4bc380eaedc59b396ecfe7a40e18c3d202e260d7
Shivanjali <shivanjalirakhunde2525@gmail.com>
2025-08-25 13:14:54 +0530

    Add Changes

--- src/Sample/simple.java
+++ src/Sample/simple.java
@@ -5,6 +5,9 @@ public class simple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello Learners!");
+		System.out.println("Hello Learners!");
+        System.out.println("Hello Batch7!");
+        System.out.println("Hello Jenkins Integration!");
 	}
 
 }
